3674af29a102fd3430dc55c2a2f301f1655cb088
AnaMolly <analia.mohamdally@gmail.com>
2021-05-24 21:26:36 -0400

    added a next question function and and an event listener to answer container. also edited some questions in html file

--- script.js
+++ script.js
@@ -1,140 +1,180 @@
 var timer = document.getElementById("countdown");
 var startButton = document.getElementById("startbutton");
 var question = document.getElementById ("question");
-var answer = document.getElementsByClassName("answer");
+var choiceA = document.getElementById("butA");
+var choiceB = document.getElementById("butB");
+var choiceC = document.getElementById("butC");
+var choiceD = document.getElementById("butD");
+var rightWrong = document.getElementById("rightwrong");
+var answerCont = document.querySelector(".answercont")
 
-var correctAnswer = true;
-var wrongAnswer = false;
-var questionsLeft = [];
+var currentQuestion = 0;
+rightWrong.innerHTML = ""
+var secondsLeft = 60;
 
 var allQuestions = [
     {
-        question: 'Inside which HTML element do we place the Javascript link?',
-        answer1: '<javascript>',
-        answer2: '<js>',
-        answer3: '<script>',
-        answer4: '<link>',
-        answer: 3,
+        question: '  Inside which HTML element do we place the Javascript link?',
+        answer1: '&lt;javascript&gt;',
+        answer2: '&lt;js&gt;',
+        answer3: '&lt;script&gt;',
+        answer4: '&lt;link&gt;',
+        correct: '&lt;script&gt;',
     },
     {
-        question: 'How do you call a Javascript function?',
+        question: '  How do you call a Javascript function?',
         answer1: 'myfunction()',
         answer2: 'myfunction{}',
         answer3: 'myfunction',
         answer4: 'myfunction[]',
-        answer: 1,
+        correct: 'myfunction()',
     },
     {
-        question: 'What is the correct syntax for referring to an external Javascript file?',
-        answer1: '<script file = "xxxx.js>',
-        answer2: '<script href = "xxxx.js>',
-        answer3: '<script alt = "xxxx.js>',
-        answer4: '<script src = "xxxx.js>',
-        answer: 4,
+        question: '  What is the correct syntax for referring to an external Javascript file?',
+        answer1: '&lt;script file = "xxxx.js&gt;',
+        answer2: '&lt;script href = "xxxx.js&gt;',
+        answer3: '&lt;script alt = "xxxx.js&gt;',
+        answer4: '&lt;script src = "xxxx.js&gt;',
+        correct: '&lt;script src = "xxxx.js&gt;',
     },
     {
-        question: 'How do you write a for loop?',
-        answer1: 'for (i=0,i<length,i++)',
-        answer2: 'for i=0,i<length, i++',
-        answer3: 'for i=0;i<length; i++',
-        answer4: 'for (i=0;i<length; i++)',
-        answer: 4,
+        question: '  How do you get an element by class when using querySelector?',
+        answer1: 'document.querySelector("class")',
+        answer2: 'document.querySelector("#class")',
+        answer3: 'document.querySelector(".class")',
+        answer4: 'document.querySelector("/class")',
+        correct: 'document.querySelector(".class")',
     },
     {
-        question: 'How do you write an if statement?',
+        question: '  How do you write an if statement?',
         answer1: 'if x=6',
         answer2: 'if (x=6)',
         answer3: 'if (x==6)',
         answer4: 'if x==6 then',
-        answer: 3,
+        correct: 'if (x==6)',
     },
     {
-        question: 'How do you get an element by ID?',
+        question: '  How do you get an element by ID?',
         answer1: 'document.id("ID")',
         answer2: 'getElementbyId("ID")',
         answer3: 'document.getElementbyID',
         answer4: 'document.getElementbyID("ID")',
-        answer: 4,
+        correct: 'document.getElementbyID("ID")',
     },
     {
-        question: 'How do you comment in a Javascript file?',
+        question: ' How do you comment in a Javascript file?',
         answer1: '// I am commenting',
         answer2: '** I am commenting',
         answer3: '!** I am commenting',
-        answer4: '<!-- I am commenting',
-        answer:1,
+        answer4: '?? I am commenting',
+        correct: '// I am commenting',
     },
     {
-        question: 'How do you comment in a Javascript file?',
-        answer1: '// I am commenting',
-        answer2: '** I am commenting',
-        answer3: '!** I am commenting',
-        answer4: '<!-- I am commenting',
-        answer:1,
-    },
-    {
-        question: 'Which operator is used in assigning a variable?',
+        question: '  Which operator is used in assigning a variable?',
         answer1: ':',
         answer2: '=',
         answer3: '==',
         answer4: '-',
-        answer:2,
+        correct: '=',
     },
     {
-        question: 'Where can you place the Javascript link in the HTML file?',
-        answer1: 'The <header> section',
-        answer2: 'Only the <body section',
-        answer3: 'The <head> or <body> section',
-        answer4: 'Only the <head> section',
-        answer:3,
+        question: '  Where can you place the Javascript link in the HTML file?',
+        answer1: 'The &lt;header&gt; section',
+        answer2: 'Only the &lt;body&gt; section',
+        answer3: 'The &lt;head&gt; or &lt;body&gt; section',
+        answer4: 'Only the &lt;head&gt; section',
+        correct: 'The &lt;head&gt; or &lt;body&gt; section',
     },
     {
-        question: 'Which built in method converts a string element to uppercase?',
+        question: '  Which built in method converts a string element to uppercase?',
         answer1: 'toUpper()',
         answer2: 'UpperCase()',
         answer3: 'change(toUpperCase)',
         answer4: 'toUpperCase()',
-        answer:4,
+        correct: 'toUpperCase()',
     },
     {
-        question: 'Which built in method removes the last element of an array?',
+        question: '  Which built-in method removes the last element of an array?',
         answer1: 'shift()',
         answer2: 'removeLast()',
         answer3: 'pop()',
         answer4: 'last()',
-        answer:3,
+        correct: 'pop()',
     },
+    {
+        question: '  Which built-in method removes the first element of an array?',
+        answer1: 'shift()',
+        answer2: 'removeLast()',
+        answer3: 'pop()',
+        answer4: 'last()',
+        correct: 'shift()',
+    }
     
     
 ]
 
-
 function startGame() {
     
     setTime();
+    nextQuestion();
+    
+    
+
+    function nextQuestion(){
+        
+        question.innerHTML = allQuestions[currentQuestion].question;
+        choiceA.innerHTML = allQuestions[currentQuestion].answer1;
+        choiceB.innerHTML = allQuestions[currentQuestion].answer2;
+        choiceC.innerHTML = allQuestions[currentQuestion].answer3;
+        choiceD.innerHTML = allQuestions[currentQuestion].answer4;
+    }    
+        
+
+    answerCont.addEventListener ("click", function(event){
+       
+        var userResponse = event.target.innerHTML;
+
+
+        if (userResponse == allQuestions[currentQuestion].correct){
+            rightWrong.innerHTML = "Correct!";
+            rightWrong.style.color = "green";
+            rightWrong.style.fontSize = "30px";
+
+        } else {
+
+            rightWrong.innerHTML = "Wrong!";
+            rightWrong.style.color = "red";
+            rightWrong.style.fontSize = "30px";
+            secondsLeft -= 15;
+        }
+        console.log(allQuestions[currentQuestion].correct)
+        
+        currentQuestion++;
+        nextQuestion();
+
+    });
 
     function setTime() {
-        var secondsLeft = 20;
+        
         startButton.style.visibility='hidden';
 
         var timerInterval = setInterval(function() {
           secondsLeft--;
           timer.textContent = secondsLeft;
-      
+
           if(secondsLeft === 0) {
             clearInterval(timerInterval);
             timer.textContent = "Time is up!";
             timer.style.color = "red";
             startButton.style.visibility='visible';
-            
           }
+
       
-        }, 500);
+        }, 800);
         
     };
-    
-    
 }
+   
 
 
 
